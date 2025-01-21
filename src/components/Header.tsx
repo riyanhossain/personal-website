d47950a3b36dbb8bd8b16e911ca316afefe35651
riyanhossain <mehraabriyan@gmail.com>
2025-01-21 18:20:20 +0600

    feat: transtiion updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Languages, Menu } from "lucide-react";
 import ModeSwitcher from "@/components/ModeSwitcher";
 import MenuComp from "@/components/MenuComp";
+import { useStore } from "@nanostores/react";
+import { isOpen } from "@/store/global";
 
 export default function Header() {
+  const $isOpen = useStore(isOpen);
+
+  const toggleMenu = () => isOpen.set(false);
   return (
     <header className="flex items-center justify-between gap-x-6 py-4 w-full max-w-2xl mx-auto px-5 lg:px-6 text-2xl z-50">
-      <a href="/" className="no-underline">
+      <a onClick={toggleMenu} href="/" className="no-underline">
         <ScrambleHover text=">_" maxIterations={7} />
       </a>
       <nav className="flex gap-x-2">
