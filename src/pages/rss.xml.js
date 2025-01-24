352b76e29e6929d762b329d876a705d15c47548b
riyanhossain <mehraabriyan@gmail.com>
2025-01-24 16:11:47 +0600

    feat: fix build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,8 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import * as constants from "@/constants";
 
+export const prerender = true;
+
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
